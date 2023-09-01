630166a05f65c56ff549fda7a615164fff283de9
Sören <74495476+1o2r3n@users.noreply.github.com>
2023-09-01 18:13:49 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,14 @@
+Config = {}
 
+-- ⭐ Default
+
+Config.Servername = 'Servername'
+Config.Maincolor = {20, 175, 12}
+
+function cNotify(title, message, color, type)
+    ESX.ShowNotification(message)
+end
+
+-- ⭐ Settings
+
+-- ⭐ Content
