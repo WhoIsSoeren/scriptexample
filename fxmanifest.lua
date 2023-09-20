7a786f9ebd99538e87083ac4dc3b26dd81acec60
Sören <74495476+1o2r3n@users.noreply.github.com>
2023-09-20 16:52:43 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,9 +7,9 @@ version '1.0.0'
 
 shared_script '@es_extended/imports.lua'
   
-client_scripts { '@NativeUILua_Reloaded/src/NativeUIReloaded.lua', '@mysql-async/lib/MySQL.lua', 'config.lua', 'lua/client.lua' }
+client_scripts { '@NativeUILua_Reloaded/src/NativeUIReloaded.lua', 'config.lua', 'lua/client.lua' }
 
-server_scripts { 'config.lua', 'lua/server.lua' }
+server_scripts { 'config.lua', 'lua/server.lua', '@mysql-async/lib/MySQL.lua' }
 
 ui_page 'ui/main.html'
 files { 'ui/main.html', 'ui/app.js' }
